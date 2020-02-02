bdbbe33007f5c5f24ff65ce840d614fb6218e6cc
preethamdhonekeni <59921220+preethamdhonekeni@users.noreply.github.com>
2020-02-02 15:28:14 +0530

    Update Web_page.py

--- Web_page.py
+++ Web_page.py
@@ -1,14 +1,24 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup                        #bs4 is the library to be imported
 import requests
 import pandas as pd
 
-URL = "https://en.wikipedia.org/wiki/List_of_largest_universities_and_university_networks_by_enrollment"
+#Enter the URL you want to Scrap from tthe Website 
+
+URL = "https://en.wikipedia.org/wiki/List_of_largest_universities_and_university_networks_by_enrollment"    
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, 'html.parser')
+
+#we need to decide which table you want to download and select the table name by using INSPECT option.
+
 table = soup.find('table', {'class' : "wikitable sortable"}).tbody
+
+#Create ROWS and COLUMNS
+
 rows = table.find_all("tr")
 columns = [v.text.replace("\n" , "") for v in table.find_all("th")]
-print(columns)
+
+#Create a DATA FRAME and write a loop to read the rows accordingly then convert and append into the dataframe and then save it into CSV fromat.
+
 df = pd.DataFrame(columns = columns)
 for i in range(1, len(rows)):
     tds = rows[i].find_all('td')
